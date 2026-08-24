c16486ccb1f7d8d729a0220cdd8484929111571d
DRATHARR <69870138+DRATHARR@users.noreply.github.com>
2026-08-24 02:14:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "📦 Перевіряємо та встановлюємо git і nodejs..."
+echo "📦 Перевіряємо та встановлюємо git і nodejs.../Checking and installing git and nodejs..."
 pkg update -y && pkg install git nodejs -y
 
-echo "📥 Завантажуємо та оновлюємо проєкт з GitHub..."
+echo "📥 Завантажуємо та оновлюємо проєкт з GitHub.../Downloading and updating the project from GitHub..."
 cd ~
 rm -rf dnd-dice
 git clone https://github.com/DRATHARR/dnd-dice.git
 cd dnd-dice
 
-echo "⚙️ Встановлюємо залежності (це може зайняти хвилину)..."
+echo "⚙️ Встановлюємо залежності (це може зайняти хвилину).../Installing dependencies (this may take a minute)..."
 npm install
 
-echo "🚀 Сервер готовий до запуску!"
+echo "🚀 Сервер готовий до запуску!/Server is ready to launch!"
 npm start
